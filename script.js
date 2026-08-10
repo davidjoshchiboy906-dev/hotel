e5243bbb748854b31f127a9d0d274e46b8683296
davidjoshchiboy906-dev <davidjoshchiboy906@gmail.com>
2026-08-10 21:40:03 +0100

    Replace old project with new version

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
-  const nav = document.getElementById("nav");
-  const navItems = document.getElementById("navItems");
-  const faBar = document.querySelector(".fa-bars");
-  
-  nav.addEventListener("click", () => {
-      faBar.classList.toggle("fa-bars");
-      faBar.classList.add("fa-times");
-      navItems.classList.toggle("hidden");
-  });
-  
-  const h1 = document.getElementById("animatedText");
-  let hue = 0;
+  const menuToggle = document.getElementById("menuToggle");
+  const navLinks = document.getElementById("navLinks");
 
-  function animate() {
-    hue = (hue + 1) % 360;
-    h1.style.color = `hsl(${hue}, 100%, 50%)`;
-    requestAnimationFrame(animate);
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", () => {
+      navLinks.classList.toggle("open");
+    });
   }
-
-  animate();
-  
 });
 
